929c474e70d3fe21ed7f331dd934efd41f8bda62
mpasokolov <mpasokolov@gmail.com>
2019-04-14 20:35:49 +0300

    Procfile fix

--- post-install-cmd.sh
+++ post-install-cmd.sh
@@ -1,6 +1,2 @@
-if [ -n "$DYNO" ]  && [ -n "$ENV" ]; then
-    php init --env=$ENV --overwrite=All
-    php yii migrate/up --interactive=0
-    php yii cache/flush-all
-    php yii cache/flush-schema --interactive=0
-fi
+php init --env=$Development --overwrite=All
+
