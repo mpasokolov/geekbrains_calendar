530d76c32e6197ef031738585e476fb987690696
mpasokolov <mpasokolov@gmail.com>
2019-04-15 20:35:26 +0300

    Procfile fix

--- post-install-cmd.sh
+++ post-install-cmd.sh
@@ -1,5 +1,6 @@
 php init --env=Production --overwrite=All
 php yii migrate/down 100 --interactive=0
 php yii migrate/up --interactive=0
+php yii migrate --migrationPath=@yii/rbac/migrations
 php yii cache/flush-all
 php yii cache/flush-schema --interactive=0
